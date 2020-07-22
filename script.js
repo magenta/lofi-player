24656499266a4ba4a0c698a73fedd461b78666b8
Vibert Thio <vibertthio@gmail.com>
2020-07-22 17:58:38 +0800

    add dragging effect to the book shelf and magenta logo

--- script.js
+++ script.js
@@ -393,6 +393,8 @@ function addImages() {
     bottom: "60%",
   });
 
+  dragElement(assets.shelfWithBooks);
+
   assets.synth = addImageToCanvasDiv("./assets/synth.png", {
     class: "large-on-hover",
     right: "25%",
@@ -624,6 +626,8 @@ function addImages() {
     left: "3%",
     top: "15%",
   });
+
+  dragElement(assets.logo);
 
   ampImg.addEventListener("click", () => {
     if (input.style.display === "none") {
@@ -1291,4 +1295,49 @@ function parseYoutubeId(url) {
 
 function getYoutubeEmbedUrlFromId(id = "EhBSXFidyUU") {
   return `https://www.youtube.com/embed/${id}?&loop=1&autoplay=1&controls=0&mute=1&vq=tiny`;
+}
+
+// drag
+function dragElement(elmnt) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
